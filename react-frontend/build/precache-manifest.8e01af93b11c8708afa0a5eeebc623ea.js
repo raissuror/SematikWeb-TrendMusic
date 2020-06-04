882086de348708359e0d920d10e843fd4cda9cb4
raissuror <raissabilullah@gmail.com>
2020-06-04 23:12:48 +0700

    deploy app

---
+++ react-frontend/build/precache-manifest.8e01af93b11c8708afa0a5eeebc623ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55a8651f35f1de3805de9a3bb8514012",
+    "url": "/index.html"
+  },
+  {
+    "revision": "490b73cfd1fec7aeeec7",
+    "url": "/static/css/main.64e8de37.chunk.css"
+  },
+  {
+    "revision": "b429c0709a5652e57d4c",
+    "url": "/static/js/2.bb4f76cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bb4f76cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "490b73cfd1fec7aeeec7",
+    "url": "/static/js/main.8b70c6b9.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
